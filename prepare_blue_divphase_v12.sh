bc35e290e82b8401d1aaea4d1ea1a8b1239aa4fd
kawauso-png <futayamagami@gmail.com>
2026-08-31 00:15:38 +0900

    Expose tolerant tracker quiet streak and skip counters

--- prepare_blue_divphase_v12.sh
+++ prepare_blue_divphase_v12.sh
@@ -71,19 +71,16 @@ if ! grep -q 'PH T{} F{} S{}' "$RUST"; then
         print "        let phase_show = if phase_arm.valid { phase_arm } else { phase_live };"
         print "        pnp::println!(\"PH T{} F{} S{}\", phase_show.transitions, phase_show.fits, phase_show.sub_count);"
         print "        pnp::println!(\"LOCK {}F V{}/{}\", phase_show.lock_prefix, phase_show.forecast_hits, phase_show.forecast_checks);"
-        print "        pnp::println!(\"OBS K{:02X} D{:02X} G{} R{}\", phase_show.last_k, phase_show.last_div_step, phase_show.last_gap, phase_show.last_reason);"
+        print "        pnp::println!(\"Q{} X{} R{}\", phase_show.quiet_streak, phase_show.rng_skips, phase_show.last_reason);"
+        print "        pnp::println!(\"OBS K{:02X} D{:02X} G{}\", phase_show.last_k, phase_show.last_div_step, phase_show.last_gap);"
     }
     { print }
     ' "$RUST" > "$RUST.tmp"
     mv "$RUST.tmp" "$RUST"
 fi
 
-# Deterministic fixes applied before lint/test/build.
-sed -i 's/let prev_sub = c.sample_sub.wrapping_sub(FRAME_SUB_STEP) & 0x3F;/let prev_sub = c.sample_sub;/' "$TRACKER"
-sed -i 's/0x102000,0x10,1411,0x303F00,0x22/0x108000,0x10,1411,0x405000,0x22/' "$TRACKER"
-
-sed -i 's/BLUE MEWTWO RNG v7.3.2 SAFE/BLUE MEWTWO RNG v7.4.1 DIVPHASE/' "$RUST"
-sed -i 's/PRED LOCKED: phase learn/DIVPHASE READ-ONLY/' "$RUST"
+sed -i 's/BLUE MEWTWO RNG v7.3.2 SAFE/BLUE MEWTWO RNG v7.4.2 DIVPHASE/' "$RUST"
+sed -i 's/PRED LOCKED: phase learn/DIVPHASE TOLERANT/' "$RUST"
 
 sed -i 's/phase_probe_begin(trigger_entry.div);/phase_probe_reset();/' "$CTRACE"
-sed -i 's/^[[:space:]]*write_phase_probe(file, &off);/    \/\* v13: memory probe retired; DIV phase tracker is Rust-only. \*\//g' "$CTRACE"
+sed -i 's/^[[:space:]]*write_phase_probe(file, &off);/    \/\* v14: memory probe retired; tolerant DIV phase tracker is Rust-only. \*\//g' "$CTRACE"

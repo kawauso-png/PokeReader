46e254e6e7dcbd0d19064b969e2807fce6bf6cd6
kawauso-png <futayamagami@gmail.com>
2026-08-31 09:50:35 +0900

    Blue legend v8.0: declare target selector bridge

--- 3gx/includes/pokereader.h
+++ 3gx/includes/pokereader.h
@@ -5,3 +5,9 @@
 void initialize();
 void run_frame();
 u32 blue_capture_target(u32 run_id);
+
+// Blue JP fixed-legend selector. 0=Mewtwo, 1=Zapdos, 2=Articuno, 3=Moltres.
+u32 host_blue_legend_target_id(void);
+u32 host_blue_legend_target_species(void);
+u32 host_blue_legend_target_level(void);
+void host_blue_legend_target_step(s32 delta);
